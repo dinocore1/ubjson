ab890d8f0acf96ad413708a6f65654df81f871bf
paul <psoucy@sciaps.com>
2016-01-25 10:29:55 -0500

    added asShortArray

--- src/main/java/com/devsmart/ubjson/UBValue.java
+++ src/main/java/com/devsmart/ubjson/UBValue.java
@@ -296,6 +296,59 @@ public abstract class UBValue implements Comparable<UBValue> {
         return retval;
     }
 
+    public short[] asShortArray() {
+        short[] retval;
+        UBArray array = asArray();
+        switch(array.getStrongType()){
+            case Int8: {
+                byte[] data = ((UBInt8Array) array).getValues();
+                retval = new short[data.length];
+                for (int i = 0; i < data.length; i++) {
+                    retval[i] = data[i];
+                }
+                break;
+            }
+
+            case Int16: {
+                retval = ((UBInt16Array) array).getValues();
+                break;
+            }
+
+            case Int32: {
+                int[] data = ((UBInt32Array) array).getValues();
+                retval = new short[data.length];
+                for (int i = 0; i < data.length; i++) {
+                    retval[i] = (short) data[i];
+                }
+                break;
+            }
+
+            case Float32: {
+                float[] data = ((UBFloat32Array) array).getValues();
+                retval = new short[data.length];
+                for (int i = 0; i < data.length; i++) {
+                    retval[i] = (short) data[i];
+                }
+                break;
+            }
+
+            case Float64: {
+                double[] data = ((UBFloat64Array) array).getValues();
+                retval = new short[data.length];
+                for (int i = 0; i < data.length; i++) {
+                    retval[i] = (short) data[i];
+                }
+                break;
+            }
+
+
+            default:
+                throw new RuntimeException("not an int32[] type");
+        }
+
+        return retval;
+    }
+
     public int[] asInt32Array() {
         int[] retval;
         UBArray array = asArray();
